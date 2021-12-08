5d9ef297cc9f1535b8079d85c1358521f74f8e5f
梦欤 <1501076885@qq.com>
2021-12-08 22:15:12 +0800

    45

--- Assets/Scripts/Test/TestMesh.cs
+++ Assets/Scripts/Test/TestMesh.cs
@@ -6,6 +6,28 @@ public class TestMesh : MonoBehaviour {
 
   private void Start() {
 
-    Debug.Log(transform.rotation);
+    StreamWriter sw = new StreamWriter("C:\\Users\\快乐的梦鱼\\Desktop\\a.txt", false);
+    sw.Write("vertices.Length: ");
+    sw.Write(mesh.vertices.Length);
+    sw.Write("\n");
+
+    foreach(Vector3 pt in mesh.vertices) {
+      sw.Write(pt.x);
+      sw.Write(",");
+      sw.Write(pt.y);
+      sw.Write(",");
+      sw.Write(pt.z);
+      sw.Write(",");
+    }
+
+    sw.Write("\ntriangles.Length: ");
+    sw.Write(mesh.triangles.Length);
+    sw.Write("\n");
+
+    foreach(int i in mesh.triangles) {
+      sw.Write(i);
+      sw.Write(",");
+    }
+    sw.Close();
   }
 }
